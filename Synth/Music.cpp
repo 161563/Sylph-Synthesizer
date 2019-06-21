c6283212340fda58d1b86fbd363c6b9bbf080ac0
Koen Buitenhuis <161563@edu.nhtv.nl>
2019-06-21 11:08:38 +0200

    [CHANGE] Removed unnecessary files

--- Synth/Music.cpp
+++
@@ -1,5 +0,0 @@
-#include "Music.h"
-
-double getNote(NOTE note) {
-	return 440. * pow(pow(2., 1. / 12.), (double)note);
-}
--- Synthesizer/Note.h
+++ Synthesizer/Note.h
@@ -2,6 +2,7 @@
 
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <string>
 #include <vector>
 #include <algorithm>
 #include "Music.h"
